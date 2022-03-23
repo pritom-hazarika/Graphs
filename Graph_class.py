196cfffd6f0aa92944014187fcf419edcb1d06de
Pritom Hazarika <hazarikapritom@yahoo.com>
2022-03-23 08:38:16 +0530

    Update Graph_class.py
    Implemented BFS

--- Graph_class.py
+++ Graph_class.py
@@ -23,9 +23,22 @@ class Graph:
         self.visited = visited
         if self.vertices[root_vertex].value not in self.visited:
             self.visited.append(self.vertices[root_vertex].value)
-            print(self.vertices[root_vertex].value)
+            print(self.vertices[root_vertex].value,end=' ')
             for vertex in self.vertices[root_vertex].edges:
                 self.dfs(vertex.value,self.visited)
+
+    def bfs(self,root):
+        self.visited = []
+        self.next_tovisit=[root]
+        while len(self.next_tovisit)!=0:
+            current = self.next_tovisit.pop(0)
+            print(current,end =' ')
+            self.visited.append(current)
+            for child in self.vertices[current].edges:
+                if child.value not in self.visited and child.value not in self.next_tovisit :
+                    self.next_tovisit.append(child.value)
+
+
 
 
 if __name__ == '__main__':
@@ -45,7 +58,10 @@ if __name__ == '__main__':
     G.add_edge('Monika','Pritty')
     G.add_edge('X','Pritty')
     G.add_edge('Sanjoy','Y')
+    print('DFS result:')
     G.dfs('Pritom')
+    print('\nBFS result:')
+    G.bfs('Pritom')
 
 
 
